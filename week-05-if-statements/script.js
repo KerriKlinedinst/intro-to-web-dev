bcf7f3c9d02a6f2c02bd01113537826779c5f4bc
@KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-02-24 23:34:54 -0500

    Update script.js

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -1,49 +1,16 @@
-// When this function is called, it takes the user's answer to the first
-// question and uses if statements to check whether it's correct.
 function submitAnswerOne() {
-  // Get answer one's input element
+  
   const answerOneInput = document.getElementById('answer-one-input');
-  // Get the text from answer one's input element
+ 
   const answerOne = answerOneInput.value;
 
-  if(answerOne == 'cat') {
-    alert("That's right!");
+  if(answerOne == 'Bellbird') {
+    alert("That's right! The Bellbird, specifically the White Bellbird has the loudest call that can get up to 116 decibels!");
     hide('question-one');
     show('question-two');
-  } else if(answerOne == 'dog') {
-    alert('Close, but my favorite animal runs away from dogs.');
+  } else if(answerOne == 'Eagle') {
+    alert('Almost. This bird is tropical. But, it needs to be more than flashy to attract a mate. It will leave your ears ringing!');
   } else {
-    alert('Wrong! Hint: My favorite animal meows.')
+    alert('Wrong answer. Bell is a contrdictary word to call this bird call.')
   }
-}
-
-// When this function is called, it takes the user's answer to the second
-// question and uses if statements to check whether it's correct.
-function submitAnswerTwo() {
-  // Get answer two's input element
-  const answerTwoInput = document.getElementById('answer-two-input');
-  // Get the number from answer two's input element
-  const answerTwo = answerTwoInput.value;
-
-  if(answerTwo == 42) {
-    alert("That's right!");
-    hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
-  } else {
-    alert('The answer is lower.')
-  }
-}
-
-// Helper function that takes an ID and shows the element with that ID.
-function show(id) {
-  const element = document.getElementById(id);
-  element.style.display = 'block';
-}
-
-// Helper  function that takes an ID and hides the element with that ID.
-function hide(id) {
-  const element = document.getElementById(id);
-  element.style.display = 'none';
 }
