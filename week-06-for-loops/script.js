65e7e7427b2d174eea0f9a62ecde4e18f93cd906
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-04 11:57:46 -0500

    Update script.js

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -3,7 +3,8 @@
 const speech = `Blackness. And a rising chill of uncertainty. Was it over? Yes! He had won. He had defeated the machine, unshackled by someone elses command.
 Freedom was mere moments away. And yet, as the immense door slowly opened, Stanley reflected on how many puzzles still lay unsolved.
 Where had his co-workers gone? How had he been freed from the machine's grasp? What other mysteries did this strange building hold?
-But, as sunlight streamed into the chamber, he realized that none of this mattered to him.`;
+But, as sunlight streamed into the chamber, he realized that none of this mattered to him. For it was not knowledge, or even power, that he had been seeking, but happiness.
+Perhaps his goal had not been to understand, but to let go. No longer would anyone tell him where to go, what to do, or how to feel.`;
 
 
 const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
