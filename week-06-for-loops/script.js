e6cacc458757959907517c18a2f28d6f4563d25a
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-03 21:32:53 -0500

    Update script.js

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,16 +1,15 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
 const speech = `Blackness... and a rising chill of uncertainty...Was it over? Yes! He had won.
-He had defeated the machine, unshackled himself from someone else's command. Freedom was mere moments away.
+He had defeated the machine, unshackled himself from someone elses command. Freedom was mere moments away.
 And, yet, even as the immense door slowly opened, Stanley reflected on how many puzzles still lay unsolved.
-Where had his co-workers gone? How had he been freed from the machine's grasp? What other mysteries did this strange building hold?
+Where had his co-workers gone? How had he been freed from the machines grasp? What other mysteries did this strange building hold?
 But as sunlight streamed into the chamber, he realized none of this mattered to him.
 For it was not knowledge, or even power, that he had been seeking, but happiness.
 Perhaps his goal had not been to understand, but to let go.
 No longer would anyone tell him where to go, what to do, or how to feel.
 Whatever life he lives, it will be his. And that was all he needed to know. It was, perhaps, the only thing worth knowing.
 Stanley stepped through the open door.`;
-
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
 const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
@@ -36,7 +35,10 @@ function displayLongWords() {
   }
 }
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 86d68df9c2179ad8819f296432655458cb30df33
 function displaySpeechStats() {
   document.getElementById('speech').innerText = speech;
 
