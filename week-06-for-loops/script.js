03afe1be264fa83d76e53e7f32fa23f01f24f050
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-04 12:43:15 -0500

    Update script.js

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -4,7 +4,8 @@ const speech = `Blackness. And a rising chill of uncertainty. Was it over? Yes! 
 Freedom was mere moments away. And yet, as the immense door slowly opened, Stanley reflected on how many puzzles still lay unsolved.
 Where had his co-workers gone? How had he been freed from the machine's grasp? What other mysteries did this strange building hold?
 But, as sunlight streamed into the chamber, he realized that none of this mattered to him. For it was not knowledge, or even power, that he had been seeking, but happiness.
-Perhaps his goal had not been to understand, but to let go. No longer would anyone tell him where to go, what to do, or how to feel.`;
+Perhaps his goal had not been to understand, but to let go. No longer would anyone tell him where to go, what to do, or how to feel.
+Whatever life he lives, it will be his. And that was all he needed to know. It was, perhaps, the only thing worth knowing. Stanley stepped through the open door.`;
 
 
 const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
