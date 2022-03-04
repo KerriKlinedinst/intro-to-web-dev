4682030e397f162526c4fabdfc4094391de51504
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-04 11:00:14 -0500

    Update script.js

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,7 +1,8 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
-const speech = `Blackness... and a rising chill of uncertainty... Was it over? Yes! He had won. He had defeated the machine, unshackled by someone else's command.
-Freedom was mere moments away. And yet, as the immense door slowly opened, Stanley`;
+const speech = `Blackness. And a rising chill of uncertainty. Was it over? Yes! He had won. He had defeated the machine, unshackled by someone else's command.
+Freedom was mere moments away. And yet, as the immense door slowly opened, Stanley Where had his co-workers gone? How had he been freed from the machine's graspe?
+What other mysteries did this strange building hold? But, as sunlight streamed into the chamber, he realized that none of this mattered to him.`;
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
 const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
