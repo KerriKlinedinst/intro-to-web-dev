76523bf27545f8b5c1358908b6d2ca747dd98802
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-18 14:15:07 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,20 +1,7 @@
-function setup() {
-  createCanvas(600, 600);
-}
-
-function draw() {
-background(100);
-stroke(255);
-
-for (let lineX = 75; lineX <= 225; lineX += 75){
-line(lineX, 0, lineX, height);
-}
-}
-
 let img;
 
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(600, 600);
   img = loadImage('IMG_1603.jpeg');
 }
 
