63759424883760f4a0289cb7236c154f3005134c
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-18 14:03:08 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -6,7 +6,7 @@ function draw() {
 background(100);
 stroke(255);
 
-for (let lineX = 75; lineX <= 225; lineX += 75){
+for (let lineX = 75; lineX <= 225; lineX <= 75){
 line(lineX, 0, lineX, height);
 }
 }
