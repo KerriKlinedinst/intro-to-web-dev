5cc7a783fcf52d6b879f047773e6340851278f90
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-20 22:21:18 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -9,21 +9,5 @@ stroke(127, 63, 120);
 // flower
  translate(200, 200);
  noStroke();
- for (let i = 0; i < 10; i ++) {
    ellipse(0, 30, 20, 40);
    rotate(PI/5);
-
-   let xpos, ypos; // Starting position
-
-   let xspeed = 3.0; // Speed
-   let yspeed = 2.8; 
-
-   let xdirection = 1; // Left or Right
-   let ydirection = 1; // Top to Bottom
-
-   if (xpos > width - rad || xpos < rad) {
-     xdirection *= -1;
-   }
-   if (ypos > height - rad || ypos < rad) {
-     ydirection *= -1;
-   }
