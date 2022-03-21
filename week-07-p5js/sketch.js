f502bf4f831d2a7b23246e815de748111c261481
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-20 22:44:02 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,13 +1,39 @@
+class Particle {
+
+  constructor(){
+    this.x = random(0, width);
+    this.y = random(0, height);
+  this.r - random(1,10);
+  this.xSpeed = random(-2,2);
+  this.ySpeed = random(-2,2.5);
+}
+
+createParticles() {
+  noStroke();
+  fill(204, 101, 192, 127);
+  ellispe(this.x,this.y,this.r);
+}
+moveParticles() {
+  if(this.x < 0 || this.x > width)
+  this.xSpeed*=-1;
+  if(this.y < 0 || this.y > height)
+  this.ySpeed*=--1;
+  this.x+=this.xSpeed;
+  this.y+=this.ySpeed;
+}
+let particles = [];
+
 function setup() {
-  createCanvas(300, 300);
-background (128, 128);
-
-  // colors
-fill(204, 101, 192, 127);
-stroke(127, 63, 120);
-
-// flower
- translate(200, 200);
- noStroke();
-   ellipse(0, 30, 20, 40);
-   rotate(PI/5);
+  createCanvas(400, 400);
+  for(let i = 0;i<width/10;i++){
+    particles.push(new Particle());
+  }
+}
+  function draw() {
+background ('#32cd32');
+for(let i = 0;i<particles.length;i++) {
+    particles[i].createParticle();
+    particles[i].moveParticle();
+    particles[i].joinParticles(particles.slice(i));
+  }
+}
