7546402b7a5df266ac52f9521bab7bf0a73fc0e1
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-20 20:14:24 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -3,11 +3,4 @@ function setup() {
 function draw() {
   background(100);
   stroke(255);
-// simple flower design
-translate(580, 200);
-noStroke();
-for (let i = 0; i < 10; i ++) {
-ellispe(0, 30, 20, 80);
-rotate(PI/5);
-}
 }
