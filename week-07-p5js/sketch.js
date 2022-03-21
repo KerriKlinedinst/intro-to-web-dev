e485d76dee3f2a871bdf996ab5638376dca2d3b4
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-20 20:54:04 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,6 +1,16 @@
 function setup() {
   createCanvas(300, 300);
-function draw() {
-  background(100);
-  stroke(255);
+  background(200, 200);
+
+  // Set colors
+fill(204, 101, 192, 127);
+stroke(127, 63, 120);
+
+// A design for a simple flower
+ translate(580, 200);
+ noStroke();
+ for (let i = 0; i < 10; i ++) {
+   ellipse(0, 30, 20, 80);
+   rotate(PI/5);
+ }
 }
