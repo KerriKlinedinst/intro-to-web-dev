f479fb0f9dee949baea40530ed2b2fabd31ba529
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-20 23:05:49 -0400

    Update sketch.js

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,25 +1,21 @@
 
 
-// this class describes the properties of a single particle.
 class Particle {
-// setting the co-ordinates, radius and the
-// speed of a particle in both the co-ordinates axes.
+
   constructor(){
     this.x = random(0,width);
     this.y = random(0,height);
     this.r = random(1,8);
-    this.xSpeed = random(-2,2);
-    this.ySpeed = random(-1,1.5);
+    this.xSpeed = random(-3,2);
+    this.ySpeed = random(-1,2);
   }
 
-// creation of a particle.
   createParticle() {
     noStroke();
-    fill('rgba(200,169,169,0.5)');
-    circle(this.x,this.y,this.r);
+    fill('rgb(255,105,180)');
+    triangle(this.x,this.y,this.r);
   }
 
-// setting the particle in motion.
   moveParticle() {
     if(this.x < 0 || this.x > width)
       this.xSpeed*=-1;
@@ -29,31 +25,28 @@ class Particle {
     this.y+=this.ySpeed;
   }
 
-// this function creates the connections(lines)
-// between particles which are less than a certain distance apart
   joinParticles(particles) {
     particles.forEach(element =>{
       let dis = dist(this.x,this.y,element.x,element.y);
       if(dis<85) {
-        stroke('rgba(255,255,255,0.04)');
+        stroke('rgb(219,112,147)');
         line(this.x,this.y,element.x,element.y);
       }
     });
   }
 }
 
-// an array to add multiple particles
 let particles = [];
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(500, 400);
   for(let i = 0;i<width/10;i++){
     particles.push(new Particle());
   }
 }
 
 function draw() {
-  background('#0f0f0f');
+  background('216,228,188');
   for(let i = 0;i<particles.length;i++) {
     particles[i].createParticle();
     particles[i].moveParticle();
