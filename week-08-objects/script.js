46f6812120125994e0594448c2af318e08f20fd0
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-24 21:54:13 -0400

    Update script.js

--- week-08-objects/script.js
+++ week-08-objects/script.js
@@ -3,14 +3,7 @@
 const taskArray = [
   {label: 'Food Editors Favorites: Treasured Reciepes - Barbara Gibbs Ostmann & Jane Baker', time: 160},
   {label: 'Magnificient Obssession - Lloyd C. Douglas', time: 330},
-  {label: "Reader's Digest Condensed Books: Number 2. 1960", time: 575},
-  {label: 'Raintree County - Ross Lockridge Jr.', time: 1066},
-  {label: 'A Place to Call Home - Cynthia Reese', time: 362},
-  {label: 'Deep Down Dark - Hector Tobar', time: 309},
-  {label: 'The Invertebrates: Volume VI Mollusca I - Libbie H. Hyman', time: 792},
-  {label: 'We, The Drowned - Carsten Jensen', time: 678},
-  {label: 'A Tale for the Time Being - Ruth Ozeki', time: 422},
-  {label: 'The Urantia Book - Author Unknown', time: 2097},
+
 ];
 
 // Loads the content into the page.
