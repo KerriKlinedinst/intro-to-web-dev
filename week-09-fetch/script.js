de893cd878b4c6968af597a5a3b0c4e3fded37ab
KerriKlinedinst <98114316+KerriKlinedinst@users.noreply.github.com>
2022-03-31 21:45:20 -0400

    Update script.js

--- week-09-fetch/script.js
+++ week-09-fetch/script.js
@@ -1,6 +1,6 @@
 // Loads content for a Wikipedia article and displays it in the page.
 async function loadContent() {
-  const pageId = 'Bird';
+  const pageId = 'Weasel';
   const response = await fetch(
     'https://en.wikipedia.org/w/api.php?' +
     'action=parse&formatversion=2&format=json&origin=*&page='
